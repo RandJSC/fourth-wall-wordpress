1493eb6f1f5e87594792c53402f03e0749b8de69
Mike Green <mike.is.green@gmail.com>
2014-08-05 16:47:39 -0400

    Added up/down sync tasks for database, but the up version isn't quite working yet

--- tasks/database.js
+++ tasks/database.js
@@ -13,10 +13,119 @@ var mysql      = require('mysql');
 var async      = require('async');
 var _          = require('lodash');
 
+var localConfig     = secrets.servers.dev;
+var remoteConfig    = secrets.servers.staging;
+
+var commandTemplate = function(parts, bindings) {
+  if (_.isString(parts)) {
+    return _.template(parts, bindings);
+  }
+
+  return _.template(parts.join(' '), bindings);
+};
+
+var remoteShell = function(command) {
+  var tpl = "ssh -p <%= port %> <%= username %>@<%= hostname %> '<%= command %>'";
+  var cmd = _.template(tpl, {
+    port: remoteConfig.ssh.port,
+    username: remoteConfig.ssh.username,
+    hostname: remoteConfig.ssh.hostname,
+    command: command
+  });
+  return shell(cmd);
+};
+
+var mysqlQuery = function(query, remote) {
+  if (_.isUndefined(remote)) {
+    remote = false;
+  }
+
+  var conf    = remote ? remoteConfig : localConfig;
+  var command = commandTemplate([
+    'mysql',
+    '--user="<%= username %>"',
+    '--password="<%= password %>"',
+    "--execute=\\'<%= query %>\\'",
+    "--database=\\'<%= database %>\\'"
+  ], {
+    query: query,
+    username: conf.mysql.username,
+    password: conf.mysql.password,
+    database: conf.mysql.database
+  });
+
+  console.log(command);
+
+  return command;
+};
+
 gulp.task('db:up', function() {
   gutil.log('Syncing database up to staging...'.blue);
 
-  gutil.log('Done!'.green);
+  var localConfig  = secrets.servers.dev;
+  var remoteConfig = secrets.servers.staging;
+
+  var mysqlDumpCmd = commandTemplate([
+    'mysqldump',
+    '--user="<%= username %>"',
+    '--password="<%= password %>"',
+    '<%= database %>',
+    '|',
+    'gzip',
+    '>',
+    '/tmp/<%= database %>.sql.gz'
+  ], {
+    username: localConfig.mysql.username,
+    password: localConfig.mysql.password,
+    database: localConfig.mysql.database
+  });
+
+  var scpCmd = commandTemplate([
+    'scp',
+    '-P <%= port %>',
+    '/tmp/<%= database %>.sql.gz',
+    '<%= username %>@<%= hostname %>:/tmp/'
+  ], {
+    port: remoteConfig.ssh.port,
+    database: localConfig.mysql.database,
+    username: remoteConfig.ssh.username,
+    hostname: remoteConfig.ssh.hostname
+  });
+
+  var loadDumpCmd = commandTemplate([
+    'zcat',
+    '/tmp/<%= database %>.sql.gz',
+    '|',
+    'mysql',
+    '--user="<%= username %>"',
+    '--password="<%= password %>"',
+    '<%= remoteDatabase %>'
+  ], {
+    database: localConfig.mysql.database,
+    username: remoteConfig.mysql.username,
+    password: remoteConfig.mysql.password,
+    remoteDatabase: remoteConfig.mysql.database
+  });
+
+  var optionsQuery = 'UPDATE wp_options SET option_value = "http://' + remoteConfig.url + '" ';
+  optionsQuery    += 'WHERE option_name = "home" OR option_name = "siteurl"';
+
+  var postGuidsQuery = 'UPDATE wp_posts SET guid = REPLACE(guid, "http://' + localConfig.url;
+  postGuidsQuery    += '", "http://';
+  postGuidsQuery    += remoteConfig.url + '")';
+
+  var postContentQuery = 'UPDATE wp_posts SET post_content = REPLACE(post_content, "http://' + localConfig.url + '" ';
+  postContentQuery    += '", "http://';
+  postContentQuery    += remoteConfig.url + '")';
+
+
+  return gulp.src('')
+    .pipe(shell(mysqlDumpCmd))
+    .pipe(shell(scpCmd))
+    .pipe(remoteShell(loadDumpCmd))
+    .pipe(remoteShell(mysqlQuery(optionsQuery, true)))
+    .pipe(remoteShell(mysqlQuery(postGuidsQuery, true)))
+    .pipe(remoteShell(mysqlQuery(postContentQuery, true)));
 });
 
 gulp.task('db:down', function() {
@@ -25,7 +134,7 @@ gulp.task('db:down', function() {
   var config    = secrets.servers.staging;
   var devConfig = secrets.servers.dev;
 
-  var mysqlDumpTpl = [
+  var mysqlDumpCmd = commandTemplate([
     'mysqldump',
     '--user="<%= username %>"',
     '--password="<%= password %>"',
@@ -34,40 +143,25 @@ gulp.task('db:down', function() {
     "gzip",
     ">",
     "/tmp/<%= database %>.sql.gz"
-  ].join(' ');
-  var mysqlDumpCmd = _.template(mysqlDumpTpl, {
+  ], {
     username: config.mysql.username,
     password: config.mysql.password,
     database: config.mysql.database
   });
 
-  var sshTpl = [
-    'ssh',
-    '-p',
-    config.ssh.port,
-    '<%= username %>@<%= hostname %>',
-    "'<%= command %>'"
-  ].join(' ');
-  var sshCmd = _.template(sshTpl, {
-    username: config.ssh.username,
-    hostname: config.ssh.hostname,
-    command: mysqlDumpCmd
-  });
-
-  var scpTpl = [
+  var scpCmd = commandTemplate([
     'scp',
     '-P',
     config.ssh.port,
     '<%= username %>@<%= hostname %>:/tmp/<%= database %>.sql.gz',
     '/tmp/'
-  ].join(' ');
-  var scpCmd = _.template(scpTpl, {
+  ], {
     username: config.ssh.username,
     hostname: config.ssh.hostname,
     database: config.mysql.database
   });
 
-  var mysqlLoadTpl = [
+  var mysqlLoadCmd = commandTemplate([
     'zcat',
     '<%= filename %>',
     '|',
@@ -75,8 +169,7 @@ gulp.task('db:down', function() {
     '--user="<%= username %>"',
     '--password="<%= password %>"',
     '<%= database %>'
-  ].join(' ');
-  var mysqlLoadCmd = _.template(mysqlLoadTpl, {
+  ], {
     filename: '/tmp/' + config.mysql.database + '.sql.gz',
     username: devConfig.mysql.username,
     password: devConfig.mysql.password,
@@ -119,10 +212,10 @@ gulp.task('db:down', function() {
   postContent += "');";
 
   return gulp.src('')
-    .pipe(shell(sshCmd))
+    .pipe(remoteShell(mysqlDumpCmd))
     .pipe(shell(scpCmd))
     .pipe(shell(mysqlLoadCmd))
-    .pipe(shell(mysqlQueryCmd(optionsQuery)))
-    .pipe(shell(mysqlQueryCmd(postGuids)))
-    .pipe(shell(mysqlQueryCmd(postContent)));
+    .pipe(shell(mysqlQuery(optionsQuery)))
+    .pipe(shell(mysqlQuery(postGuids)))
+    .pipe(shell(mysqlQuery(postContent)));
 });

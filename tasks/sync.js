786f3c1b1e6e33343a6167c51c16141287774f81
Mike Green <mike.is.green@gmail.com>
2014-08-04 21:41:23 -0400

    Sync task works now

--- tasks/sync.js
+++ tasks/sync.js
@@ -1,32 +1,41 @@
 var gulp   = require('gulp');
 var colors = require('colors');
-var spawn  = require('child_process').spawn;
 var path   = require('path');
+var rsync  = require('rsyncwrapper').rsync;
+var gutil  = require('gulp-util');
 
 gulp.task('sync:up', function() {
   console.log('Syncing Up...'.blue);
 
-  var subdomain = 'fourthwall.fifthroomhosting.com';
-  var themeSlug = 'fourthwall';
-  var buildDir  = path.join(__dirname, '..', 'build/');
-  var rsync     = spawn('rsync', [
-    '-avzr',
-    '-e',
-    '"ssh -p 31173"',
-    '--delete',
-    buildDir,
-    'frcweb@fifthroomcreative.com:public_html/' + subdomain + '/public/wp-content/themes' + themeSlug + '/'
-  ]);
+  var subdomain  = 'fourthwall.fifthroomhosting.com';
+  var themeSlug  = 'fourthwall';
+  var buildDir   = path.join(__dirname, '..', 'build/');
+  var remotePath = path.join(
+    'frcweb@fifthroomcreative.com:public_html',
+    subdomain,
+    'public',
+    'wp-content',
+    'themes',
+    themeSlug
+  );
 
-  rsync.stdout.on('data', function(data) {
-    console.log(data);
+  rsync({
+    src: buildDir,
+    dest: remotePath,
+    ssh: true,
+    port: 31173,
+    recursive: true,
+    args: [ '--verbose' ],
+    onStdout: function(data) {
+      gutil.log(data.toString());
+    },
+    onStderr: function(data) {
+      gutil.log(data.toString());
+    }
+  }, function(err) {
+    if (err !== null) {
+      console.error(err);
+    }
   });
 
-  rsync.stderr.on('data', function(data) {
-    console.log(data.toString().red);
-  });
-
-  rsync.on('close', function(code) {
-    console.log("rsync exited with status " + code);
-  });
 });

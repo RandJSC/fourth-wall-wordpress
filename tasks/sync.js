d2692f6a0061b9668673b04e22b080f3a99cca6f
Mike Green <mike.is.green@gmail.com>
2014-08-06 14:49:30 -0400

    using commandTemplate helper in sync task

--- tasks/sync.js
+++ tasks/sync.js
@@ -8,26 +8,20 @@ var colors  = require('colors');
 var path    = require('path');
 var shell   = require('gulp-shell');
 var _       = require('lodash');
+var helpers = require('./lib/helpers.js');
 
 gulp.task('sync:up', function() {
   console.log('Syncing Up...'.blue);
 
   var config     = secrets.servers.staging.rsync;
   var buildDir   = path.join(__dirname, '..', 'build/');
-  var commandTpl = [
+  var rsyncCmd   = helpers.commandTemplate([
     'rsync -avzr',
-    '-e "ssh -p <%= port %>"',
+    '-e "ssh -p <%= staging.ssh.port %>"',
     '--delete',
     '<%= buildDir %>',
-    '<%= username %>@<%= hostname %>:<%= path %>'
-  ].join(' ');
-  var rsyncCmd   = _.template(commandTpl, {
-    port: config.port,
-    buildDir: buildDir,
-    username: config.username,
-    hostname: config.hostname,
-    path: config.path
-  });
+    '<%= staging.rsync.username %>@<%= staging.rsync.hostname %>:<%= staging.rsync.path %>'
+  ], { buildDir: buildDir });
 
   return gulp.src('', { read: false })
     .pipe(shell(rsyncCmd));

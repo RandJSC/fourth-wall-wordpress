09e430f1133a7a82f96062b29447b035e9d1d099
Mike Green <mike.is.green@gmail.com>
2014-08-06 14:50:53 -0400

    Removed extraneous require from sync task file

--- tasks/sync.js
+++ tasks/sync.js
@@ -7,15 +7,14 @@ var gulp    = require('gulp');
 var colors  = require('colors');
 var path    = require('path');
 var shell   = require('gulp-shell');
-var _       = require('lodash');
 var helpers = require('./lib/helpers.js');
 
 gulp.task('sync:up', function() {
   console.log('Syncing Up...'.blue);
 
-  var config     = secrets.servers.staging.rsync;
-  var buildDir   = path.join(__dirname, '..', 'build/');
-  var rsyncCmd   = helpers.commandTemplate([
+  var config   = secrets.servers.staging.rsync;
+  var buildDir = path.join(__dirname, '..', 'build/');
+  var rsyncCmd = helpers.commandTemplate([
     'rsync -avzr',
     '-e "ssh -p <%= staging.ssh.port %>"',
     '--delete',

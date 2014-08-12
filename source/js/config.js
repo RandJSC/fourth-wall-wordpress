1b36785711598fe013674c5040785a3ed1a719ed
Mike Green <mike.is.green@gmail.com>
2014-08-12 11:28:44 -0400

    Created a config module to store JS settings

---
+++ source/js/config.js
@@ -0,0 +1,35 @@
+/**
+ * Fourth Wall Events
+ * Global Settings and Configuration
+ */
+
+define([], function() {
+
+  var config = {};
+
+  config.defaults = {
+    animation: {
+      speed: 350
+    }
+  };
+
+  config.svg = {
+    paths: {
+      burger: [
+        'M1.97 2.122h30.127v.127H1.97z',
+        'M1.97 12.994h30.126v.127H1.92z',
+        'M1.97 23.916H32.11v.127H1.983z'
+      ],
+      close: [
+        'M5.058 3.027l22.637 19.88-.083.095L4.974 3.122z',
+        'M5.432 3.468l22.72 19.782-.083.096L5.347 3.563z',
+        'M5.36 22.796L28.42 3.408l.08.097-23.06 19.39z'
+      ]
+    },
+    animation: {
+      speed: 350
+    }
+  };
+
+  return config;
+});

8b9398115f9139b3020e4e220053af8c40328265
Mike Green <mike.is.green@gmail.com>
2014-09-10 16:51:05 -0400

    working on background images for quick links

--- source/front-page.php
+++ source/front-page.php
@@ -28,7 +28,7 @@ get_header();
               $img_src = ($idx === 0) ? ' src="' . $src[0] . '"' : '';
               $data_lazy = ($idx > 0) ? ' data-lazy="' . $src[0] . '"' : '';
               ?>
-              <img<?php echo $img_src . $data_lazy; ?> alt="<?php echo $title; ?>">
+              <img<?php echo $img_src . $data_lazy; ?> alt="<?php echo $title; ?>" width="100%">
               <figcaption><?php echo $title; ?></figcaption>
             </a>
           </figure>

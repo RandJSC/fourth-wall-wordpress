ab8295c5dc77bdd2c939f08dd4ad41961bf41ee9
Mike Green <mike.is.green@gmail.com>
2014-09-11 11:37:30 -0400

    shortened var in custom mime type filter function

--- source/includes/functions/file-types.php
+++ source/includes/functions/file-types.php
@@ -4,10 +4,10 @@
  * Allowable Upload MIME Types
  */
 
-function fwe_add_custom_mime_types($existing_mimes = array()) {
-  $existing_mimes['svg'] = 'image/svg+xml';
-
-  return $existing_mimes;
+function fwe_add_custom_mime_types($mimes = array()) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
 }
 add_filter('upload_mimes', 'fwe_add_custom_mime_types');
+
 ?>

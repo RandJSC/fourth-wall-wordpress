9974b733b79e06b6aafb152151a72a2ee1dd6c64
Mike Green <mike.is.green@gmail.com>
2014-09-11 11:34:47 -0400

    DRYed up the writing of sync tasks

--- tasks/sync.js
+++ tasks/sync.js
@@ -9,36 +9,54 @@ var path    = require('path');
 var shell   = require('gulp-shell');
 var helpers = require('./lib/helpers.js');
 
-gulp.task('sync:up', function() {
-  console.log(chalk.blue( 'Syncing Up...' ));
-
+var syncTask = function syncTask(opts) {
   var config   = secrets.servers.staging.rsync;
-  var buildDir = path.join(__dirname, '..', 'build/');
-  var rsyncCmd = helpers.commandTemplate([
+  opts         = (typeof opts === 'undefined') ? {} : opts;
+  opts.source  = opts.source ? opts.source : path.join(__dirname, '..', 'build');
+  opts.dest    = opts.dest ? opts.dest : config.path;
+  opts.message = opts.message ? opts.message : 'Beginning sync task';
+  opts.direction = ( opts.direction === 'down' || opts.direction === 'up' ) ? opts.direction : 'up';
+
+  var rsyncLines = [
     'rsync -avzr',
     '-e "ssh -p <%= staging.ssh.port %>"',
-    '--delete',
-    '<%= buildDir %>',
-    '<%= staging.rsync.username %>@<%= staging.rsync.hostname %>:<%= staging.rsync.path %>'
-  ], { buildDir: buildDir });
+    '--delete'
+  ];
 
-  return gulp.src('', { read: false })
-    .pipe(shell(rsyncCmd));
-});
+  if (opts.direction === 'up') {
+    rsyncLines.push('<%= source %>');
+    rsyncLines.push('<%= staging.rsync.username %>@<%= staging.rsync.hostname %>:<%= dest %>');
+  } else {
+    rsyncLines.push('<%= staging.rsync.username %>@<%= staging.rsync.hostname %>:<%= source %>');
+    rsyncLines.push('<%= dest %>');
+  }
 
-gulp.task('uploads:up', function() {
-  console.log(chalk.blue('Syncing uploads UP...'));
-  
-  var config     = secrets.servers.staging.rsync;
-  var uploadsDir = '/home/vagrant/www/wp-content/uploads/';
-  var rsyncCmd   = helpers.commandTemplate([
-    'rsync -avzr',
-    '-e "ssh -p <%= staging.ssh.port %>"',
-    '--delete',
-    '<%= uploadsDir %>',
-    '<%= staging.rsync.username %>@<%= staging.rsync.hostname %>:<%= staging.rsync.public_html %>/wp-content/uploads/'
-  ], { uploadsDir: uploadsDir });
-  
-  return gulp.src('', { read: false })
-    .pipe(shell(rsyncCmd));
-});
+  return function() {
+    console.log(chalk.blue(opts.message));
+
+    var rsyncCmd = helpers.commandTemplate(rsyncLines, {
+      source: opts.source,
+      dest: opts.dest
+    });
+
+    return gulp.src('', { read: false })
+      .pipe(shell(rsyncCmd));
+  };
+};
+
+gulp.task('sync:up', syncTask({
+  message: 'Syncing UP...'
+}));
+
+gulp.task('uploads:up', syncTask({
+  source: '/home/vagrant/www/wp-content/uploads/',
+  dest: 'public_html/fourthwall.fifthroomhosting.com/public/wp-content/uploads/',
+  message: 'Syncing uploads UP...'
+}));
+
+gulp.task('uploads:down', syncTask({
+  source: 'public_html/fourthwall.fifthroomhosting.com/public/wp-content/uploads/',
+  dest: '/home/vagrant/www/wp-content/uploads/',
+  message: 'Syncing uploads DOWN...',
+  direction: 'down'
+}));

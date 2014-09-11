29476d3c2883579096470be6754d94b9d4be03d2
Mike Green <mike.is.green@gmail.com>
2014-09-11 15:02:56 -0400

    Added meta-boxes for page stitching and custom background images

---
+++ source/piklist/parts/meta-boxes/page-background-image-meta-box.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Title: Page Background
+Post Type: page
+Context: normal
+Priority: high
+*/
+
+piklist('field', array(
+  'type' => 'file',
+  'field' => 'background_image',
+  'scope' => 'post_meta',
+  'label' => 'Background Image',
+  'description' => 'This image will be used as the page\'s background, stretched to fill the width of the user\'s screen. For best results, use an SVG.'
+));
+
+piklist('field', array(
+  'type'        => 'colorpicker',
+  'field'       => 'background_color',
+  'scope'       => 'post_meta',
+  'label'       => 'Background Color',
+  'description' => 'The background color shown if the image is too small for the page. Leave blank for transparent.',
+));
+
+piklist('field', array(
+  'type'        => 'text',
+  'field'       => 'background_size',
+  'scope'       => 'post_meta',
+  'label'       => 'Background Size',
+  'description' => 'Supports any valid CSS value for <code>background-size</code>. This can be used to "zoom" a background image to a specific size. Leave blank for a default 1X zoom level.',
+));
+
+piklist('field', array(
+  'type'        => 'text',
+  'field'       => 'background_position',
+  'scope'       => 'post_meta',
+  'label'       => 'Background Position',
+  'description' => 'Supports any valid CSS value for <code>background-position</code>. Leave blank to center the image.'
+));
+
+piklist('field', array(
+  'type'        => 'radio',
+  'field'       => 'background_repeat',
+  'scope'       => 'post_meta',
+  'label'       => 'Background Repeat',
+  'description' => 'Is the background image a repeating pattern?',
+  'choices'     => array(
+    'repeat'    => 'Repeat',
+    'no-repeat' => 'No Repeat'
+  ),
+));
+
+?>
---
+++ source/piklist/parts/meta-boxes/page-stitching-meta-box.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Title: Page Stitching
+Post Type: page
+Context: normal
+Priority: high
+*/
+
+global $post;
+
+$pages_args = array(
+  'sort_column' => 'post_date',
+  'sort_order'  => 'desc',
+  'hierarchical' => 0,
+);
+
+if (property_exists($post, 'ID') && $post->ID) {
+  $pages_args['exclude'] = array($post->ID);
+}
+
+$pages     = get_pages($pages_args);
+$page_opts = array();
+
+foreach ($pages as $idx => $page) {
+  $page_opts[$page->ID] = $page->post_title;
+}
+
+piklist('field', array(
+  'type'        => 'select',
+  'scope'       => 'post_meta',
+  'field'       => 'stitch_children',
+  'label'       => 'Pages to Display',
+  'description' => 'The selected pages will be shown below this page\'s content on the site. Drag and drop to change the order.',
+  'attributes'  => array('class' => 'text'),
+  'choices'     => $page_opts,
+  'add_more'    => true,
+));
+?>

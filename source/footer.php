aaf9de2cd6cdbcad437f54af1b740f82a5c9f9fb
Mike Green <mike.is.green@gmail.com>
2014-09-14 13:13:10 -0400

    Moved CTA fetch into its own function

--- source/footer.php
+++ source/footer.php
@@ -1,24 +1,8 @@
         <?php
-        global $post;
+        $cta = fwe_get_cta();
 
-        if ($post && property_exists($post, 'ID')) {
-          $post_id = $post->ID;
-          $cta_id  = get_post_meta($post_id, 'call_to_action', true);
-          $cta     = get_post($cta_id);
-        } else {
-          $cta = get_posts(array(
-            'posts_per_page' => 1,
-            'orderby'        => 'rand',
-            'post_type'      => 'call_to_action',
-            'post_status'    => 'publish',
-          ));
-
-          if (count($cta) > 0) {
-            $cta = $cta[0];
-          }
-        }
-
-        if (isset($cta)):
+        if ($cta):
+          var_dump($cta);
         ?>
 
         <?php endif; ?>
--- source/includes/functions/utilities.php
+++ source/includes/functions/utilities.php
@@ -4,4 +4,30 @@
  * Utility Functions
  */
 
+function fwe_get_cta() {
+  global $post;
+
+  if ($post && property_exists($post, 'ID')) {
+    $post_id = $post->ID;
+    $cta_id  = get_post_meta($post_id, 'call_to_action', true);
+    $cta     = get_post($cta_id);
+  } else {
+    $cta = get_posts(array(
+      'posts_per_page' => 1,
+      'orderby'        => 'rand',
+      'post_type'      => 'call_to_action',
+      'post_status'    => 'publish',
+    ));
+
+    if (count($cta) > 0) {
+      $cta = $cta[0];
+    }
+  }
+
+  if (isset($cta)) {
+    return $cta;
+  }
+
+  return null;
+}
 ?>

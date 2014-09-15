535db0d7ab95da6db25c7d996689577b9c9e86c3
Mike Green <mike.is.green@gmail.com>
2014-09-15 12:56:24 -0400

    Done with CTA for now, moving to socials

--- source/footer.php
+++ source/footer.php
@@ -1,40 +1,44 @@
-        <?php
-        $cta = fwe_get_cta();
+          <?php
+          $cta = fwe_get_cta();
 
-        if ($cta):
-          $bg_img    = get_post_meta($cta->ID, 'background_image', true);
-          $bg_img    = wp_get_attachment_image_src($bg_img, 'full');
-          $bg_size   = get_post_meta($cta->ID, 'background_size', true);
-          $bg_pos    = get_post_meta($cta->ID, 'background_position', true);
-          $bg_repeat = get_post_meta($cta->ID, 'background_repeat', true);
-          $bg_color  = get_post_meta($cta->ID, 'background_color', true);
-          $link_url  = get_post_meta($cta->ID, 'link_url', true);
-          $btn_txt   = get_post_meta($cta->ID, 'button_text', true);
-          $target    = fwe_is_url_local($link_url) ? '' : ' target="_blank"';
+          if ($cta):
+            $bg_img    = get_post_meta($cta->ID, 'background_image', true);
+            $bg_img    = wp_get_attachment_image_src($bg_img, 'full');
+            $bg_size   = get_post_meta($cta->ID, 'background_size', true);
+            $bg_pos    = get_post_meta($cta->ID, 'background_position', true);
+            $bg_repeat = get_post_meta($cta->ID, 'background_repeat', true);
+            $bg_color  = get_post_meta($cta->ID, 'background_color', true);
+            $link_url  = get_post_meta($cta->ID, 'link_url', true);
+            $btn_txt   = get_post_meta($cta->ID, 'button_text', true);
+            $target    = fwe_is_url_local($link_url) ? '' : ' target="_blank"';
 
-          $style_attr = fwe_style_attribute(array(
-            'background-image'    => "url('{$bg_img[0]}')",
-            'background-size'     => $bg_size,
-            'background-position' => $bg_pos,
-            'background-repeat'   => $bg_repeat,
-            'background-color'    => $bg_color,
-          ));
-        ?>
-          <div class="call-to-action"<?php echo $style_attr; ?>>
-            <div class="cta-body">
-              <div class="cta-contents">
-                <?php echo apply_filters('the_content', $cta->post_content); ?>
+            $style_attr = fwe_style_attribute(array(
+              'background-image'    => "url('{$bg_img[0]}')",
+              'background-size'     => $bg_size,
+              'background-position' => $bg_pos,
+              'background-repeat'   => $bg_repeat,
+              'background-color'    => $bg_color,
+            ));
+          ?>
+            <div class="call-to-action"<?php echo $style_attr; ?>>
+              <div class="cta-body">
+                <div class="cta-contents">
+                  <?php echo apply_filters('the_content', $cta->post_content); ?>
 
-                <?php if (!empty($btn_txt) && !empty($link_url)): ?>
-                  <a class="button center block" href="<?php echo esc_url($link_url); ?>"<?php echo $target; ?>>
-                    <?php echo $btn_txt; ?>
-                  </a>
-                <?php endif; ?>
+                  <?php if (!empty($btn_txt) && !empty($link_url)): ?>
+                    <a class="button center block" href="<?php echo esc_url($link_url); ?>"<?php echo $target; ?>>
+                      <?php echo $btn_txt; ?>
+                    </a>
+                  <?php endif; ?>
+                </div>
               </div>
             </div>
-          </div>
-        <?php endif; ?>
+          <?php endif; ?>
         </div><!-- /#main-content -->
+
+        <div id="socials">
+        </div>
+
         <footer id="footer-main">
           <?php
           $year = strftime('%Y');
@@ -43,6 +47,7 @@
             Copyright &copy; <?php echo $year; ?> Fourth Wall Events, All Rights Reserved.
           </p>
         </footer><!-- /#footer-main -->
+
       </div><!-- /.content-inner -->
     </div><!-- /.content -->
   </div><!-- /#master-container -->

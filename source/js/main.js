56aeeb81ce954e39ad66ccc6841bfea4fccdc8f0
Mike Green <mike.is.green@gmail.com>
2014-09-17 01:18:58 -0400

    when mobile nav is open, add a swipe handler to main container to close it

--- source/js/main.js
+++ source/js/main.js
@@ -14,21 +14,33 @@ require([
   'FastClick',
   'Snap',
   'bragi',
+  'hammerjs',
   'jquery.fourthwall-util',
   'jquery.fourthwall-ui',
   'slick'
-], function(doc, config, $, FastClick, Snap, logger) {
+], function(doc, config, $, FastClick, Snap, logger, Hammer) {
 
   logger.log('timing', 'Begin docReady');
 
-  var $container = $('#master-container');
-  var $hamburger = $('#hamburger');
-  var burgerSvg  = Snap('#hamburger-img');
+  var $container   = $('#master-container');
+  var $hamburger   = $('#hamburger');
+  var burgerSvg    = Snap('#hamburger-img');
+  var hammertime   = new Hammer.Manager($container[0], {});
+  var canSwipe     = function canSwipe(recognizer, input) {
+    return $container.hasClass('menu-open');
+  };
 
   // Attach fastclick to body
   FastClick.attach(doc.body);
 
   $hamburger.toggleNav($container, burgerSvg);
+  
+  hammertime.add(new Hammer.Swipe({ enable: canSwipe }));
+  hammertime.on('swiperight', function(evt) {
+    $hamburger.trigger('click');
+    logger.log('touch', 'swiperight');
+    console.debug(evt);
+  });
 
   $('.slider .slides').slick({
     lazyLoad: 'ondemand',

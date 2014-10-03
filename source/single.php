7604e933e22d1c2d9bae687b17349537b25842da
Mike Green <mike.is.green@gmail.com>
2014-10-03 16:23:04 -0400

    Added quicklinks and main content section for blog + news entries

--- source/single.php
+++ source/single.php
@@ -6,29 +6,53 @@
 
 get_header();
 
-global $post;
+global $post, $fwe_settings;
 
 if (isset($post) && fwe_is_post($post)) {
   $categories = get_the_category($post->ID);
 
   if (count($categories) > 0) {
-    $main_cat = $categories[0];
-    $banner   = get_term_meta($main_cat->term_id, 'banner_image', true);
+    $banner = get_term_meta($categories[0]->term_id, 'banner_image', true);
   } else {
-    // [todo] - Add default banner theme option and pull it in here.
+    $banner = $fwe_settings['default_category_banner'];
   }
+
+  $banner = $banner ? $banner : $fwe_settings['default_category_banner'];
+  $banner = wp_get_attachment_image_src($banner, 'full');
 }
 ?>
 
-<?php if (isset($banner)): ?>
+<?php if ($banner): ?>
+  <?php
+  $caption = get_term_meta($categories[0]->term_id, 'banner_caption', true);
+  $caption = !empty($caption) ? $caption : $categories[0]->name;
+  ?>
   <section class="banner">
-    <?php var_dump($banner); ?>
-    <figure>
-      <img src="" alt="">
-      <figcaption></figcaption>
+    <figure id="page-banner">
+      <img src="<?php echo $banner[0]; ?>" alt="<?php echo $caption; ?>">
+      <figcaption><?php echo $caption; ?></figcaption>
     </figure>
   </section>
 <?php endif; ?>
+
+<section id="quick-links" class="posts">
+  <?php
+  wp_nav_menu(array(
+    'theme_location' => 'post_quick_links',
+    'container'      => 'nav',
+    'fallback_cb'    => false,
+    'walker'         => new Walker_FWE_QuickLinks(),
+  ));
+  ?>
+</section>
+
+<?php if (have_posts()): while (have_posts()): the_post(); ?>
+  <section id="main-page-content" class="content-section padded">
+    <h1><?php the_title(); ?></h1>
+
+    <?php the_content(); ?>
+  </section>
+<?php endwhile; endif; ?>
 
 <?php
 get_footer();

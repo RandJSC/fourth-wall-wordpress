a72c247443d28d61b5a5e8ec825edb7b40112faa
Mike Green <mike.is.green@gmail.com>
2014-10-20 13:53:33 -0400

    Added menu slot for Case Study/Gallery QuickLinks

--- source/includes/functions/menus.php
+++ source/includes/functions/menus.php
@@ -8,11 +8,12 @@ function fwe_register_nav_menus() {
   if (!function_exists('register_nav_menus')) return;
 
   register_nav_menus(array(
-    'main_nav'         => 'Main Navigation',
-    'quick_links'      => 'QuickLinks',
-    'post_quick_links' => 'Blog/News QuickLinks',
-    'social_links'     => 'Social Links',
-    'footer_nav'       => 'Footer Navigation',
+    'main_nav'            => 'Main Navigation',
+    'quick_links'         => 'QuickLinks',
+    'post_quick_links'    => 'Blog/News QuickLinks',
+    'gallery_quick_links' => 'Gallery/Case Study QuickLinks',
+    'social_links'        => 'Social Links',
+    'footer_nav'          => 'Footer Navigation',
   ));
 }
 add_action('init', 'fwe_register_nav_menus');

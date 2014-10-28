80075a48f5f419fa73dfd9ae18a5908be9b87a25
Mike Green <mike.is.green@gmail.com>
2014-10-27 20:39:50 -0400

    Accidentally broke homepage slider a few commits ago. Fixed it.

--- source/front-page.php
+++ source/front-page.php
@@ -26,7 +26,7 @@ get_header();
           <figure class="slide">
             <a href="<?php echo $link_url; ?>">
               <?php
-              $img_src = ($idx === 0) ? 'src="' . $src[0] . '"' : 'data-lazy="' . $src[0] . '"';
+              $img_src = ($idx === 0) ? ' src="' . $src[0] . '"' : 'data-lazy="' . $src[0] . '"';
               ?>
               <img<?php echo $img_src; ?> alt="<?php echo $title; ?>" width="100%">
               <figcaption><?php echo $title; ?></figcaption>

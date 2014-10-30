a8f9694e1867d2db9ea7f3729024acb6a84c974a
Mike Green <mike.is.green@gmail.com>
2014-10-30 14:54:30 -0400

    pretty sure I have all the page stitching markup done correctly

--- source/page.php
+++ source/page.php
@@ -9,8 +9,65 @@ $banner = fwe_get_page_banner($post->ID);
 <?php get_template_part('subpage', 'banner'); ?>
 
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
+  <?php
+  // Fetch the pages whose stitch_content should be pulled into this one:
+  $post_id  = get_the_ID();
+  $stitches = get_post_meta($post_id, 'stitch_children');
+  $stitches = array_filter($stitches, function($item) {
+    return !empty($item);
+  });
+  ?>
 
+  <?php // This page's content: ?>
   <?php get_template_part('subpage', 'main-content'); ?>
+
+  <?php
+  // Stitched subpage content (if any):
+
+  if (is_array($stitches) && count($stitches)):
+    foreach ($stitches as $stitch_id):
+      $post = get_post($stitch_id);
+      $stitch_content = get_post_meta($stitch_id, 'stitch_content', true);
+      $stitch_content = apply_filters('the_content', $stitch_content);
+      $bg_image       = get_post_meta($stitch_id, 'background_image', true);
+      $bg_color       = get_post_meta($stitch_id, 'background_color', true);
+      $bg_size        = get_post_meta($stitch_id, 'background_size', true);
+      $bg_pos         = get_post_meta($stitch_id, 'background_position', true);
+      $bg_repeat      = get_post_meta($stitch_id, 'background_repeat', true);
+
+      $section_style  = fwe_style_attribute(array(
+        'background-image'    => $bg_image,
+        'background-size'     => $bg_size,
+        'background-position' => $bg_pos,
+        'background-repeat'   => $bg_repeat,
+      ));
+      $article_style  = fwe_style_attribute(array(
+        'background-color' => $bg_color,
+      ));
+
+      setup_postdata($post);
+  ?>
+      <section
+        class="stitch"
+        data-slug="<?php echo $post->post_name; ?>"
+        data-url="<?php the_permalink(); ?>"<?php echo $section_style; ?>>
+
+        <article <?php post_class(); ?><?php echo $article_style; ?>>
+          <div class="post-header padded">
+            <h2><?php the_title(); ?></h2>
+            <hr>
+          </div>
+
+          <div class="post-content padded">
+            <?php echo $stitch_content; ?>
+          </div>
+        </article>
+      </section>
+  <?php
+    endforeach;
+    wp_reset_postdata();
+  endif;
+  ?>
 
 <?php endwhile; endif; ?>
 

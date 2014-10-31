1a5711e1a110645f84ff095a3a38bccd321861fd
Mike Green <mike.is.green@gmail.com>
2014-10-31 14:18:22 -0400

    Added accordion panes metabox to accordion CPT

---
+++ source/piklist/parts/meta-boxes/accordion-meta-box.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Title: Accordion
+Post Type: accordion
+Context: normal
+Priority: high
+*/
+
+piklist('field', array(
+  'type'     => 'group',
+  'field'    => 'accordion_panes',
+  'label'    => 'Accordion Panes',
+  'add_more' => true,
+  'fields'   => array(
+    array(
+      'type'  => 'text',
+      'field' => 'title',
+      'label' => 'Title',
+    ),
+    array(
+      'type'        => 'editor',
+      'field'       => 'content',
+      'label'       => 'Content',
+      'description' => 'Things will explode if you try to embed another accordion here.',
+      'teeny' => true,
+    ),
+  ),
+));
+?>

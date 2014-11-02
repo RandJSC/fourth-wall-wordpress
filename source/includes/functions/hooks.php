3aff03644704714f99f73dbfde732b781dd40666
Mike Green <mike.is.green@gmail.com>
2014-11-02 14:14:46 -0500

    removed unused post save hook

--- source/includes/functions/hooks.php
+++ source/includes/functions/hooks.php
@@ -4,8 +4,4 @@
  * Post Save Hooks
  */
 
-function fwe_update_location($post_id) {
-  if (!array_key_exists('post_type', $_POST) || $_POST['post_type'] !== 'location') return;
-}
-add_action('save_post', 'fwe_update_location');
 ?>

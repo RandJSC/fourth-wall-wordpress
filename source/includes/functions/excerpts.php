eaef1eb1d8c633c75e4648b5b3afc2fd71175b3c
Mike Green <mike.is.green@gmail.com>
2014-11-13 11:48:42 -0500

    Removed unused excerpt more code

--- source/includes/functions/excerpts.php
+++ source/includes/functions/excerpts.php
@@ -10,11 +10,6 @@ function fwe_excerpt_length($length) {
 add_filter('excerpt_length', 'fwe_excerpt_length', 999);
 
 function fwe_excerpt_more($more) {
-  /*
-  global $post;
-  $link = get_permalink($post->ID);
-  return '&hellip;&nbsp;<a href="' . $link . '">Read More &raquo;</a>';
-  */
   return '&hellip;';
 }
 add_filter('excerpt_more', 'fwe_excerpt_more');

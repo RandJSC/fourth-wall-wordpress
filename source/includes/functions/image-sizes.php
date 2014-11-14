9d731467cffa40a76b44ffb5516e54bc3b6aeddd
Mike Green <mike.is.green@gmail.com>
2014-11-14 14:07:44 -0500

    Removed team members page - using shortcode instead probably. Added thumbnail image size for team members

--- source/includes/functions/image-sizes.php
+++ source/includes/functions/image-sizes.php
@@ -13,6 +13,7 @@ function fwe_image_sizes() {
 
   add_image_size('tiny-thumb', 95, 75, true);
   add_image_size('team-member-headshot', 192, 192, true);
+  add_image_size('team-member-thumb', 124, 124, true);
   add_image_size('homepage-slider', 1856, 720, true);
 }
 add_action('init', 'fwe_image_sizes');

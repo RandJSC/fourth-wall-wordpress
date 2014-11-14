d3ed51da05099e6d6ff5285d4b382ff7c43dd801
Mike Green <mike.is.green@gmail.com>
2014-11-14 12:30:32 -0500

    Pulling custom contact info fields into team member JSON output in API

--- source/includes/api/team-members.php
+++ source/includes/api/team-members.php
@@ -11,8 +11,25 @@ class FourthWall_TeamMembers extends WP_JSON_CustomPostType {
 
   public function register_routes($routes) {
     $routes = parent::register_routes($routes);
-
     return $routes;
+  }
+
+  protected function prepare_post($post, $context = 'view') {
+    $post = parent::prepare_post($post, $context);
+
+    // Add custom fields
+    $post['surname']       = get_post_meta($post['ID'], 'surname', true);
+    $post['job_title']     = get_post_meta($post['ID'], 'job_title', true);
+    $post['location']      = get_post_meta($post['ID'], 'location', true);
+    $post['email_address'] = get_post_meta($post['ID'], 'email_address', true);
+    $post['facebook']      = get_post_meta($post['ID'], 'facebook', true);
+    $post['twitter']       = get_post_meta($post['ID'], 'twitter', true);
+    $post['instagram']     = get_post_meta($post['ID'], 'instagram', true);
+    $post['linkedin']      = get_post_meta($post['ID'], 'linkedin', true);
+    $post['pinterest']     = get_post_meta($post['ID'], 'pinterest', true);
+    $post['google_plus']   = get_post_meta($post['ID'], 'google_plus', true);
+
+    return $post;
   }
 
 }

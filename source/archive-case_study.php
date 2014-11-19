830b707526edc9f0e965c33c82d0640619c33aaa
Mike Green <mike.is.green@gmail.com>
2014-11-19 16:15:25 -0500

    started on templates for case study and gallery archives

---
+++ source/archive-case_study.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Fourth Wall Events
+ * Case Study Archive Template
+ */
+
+get_header();
+
+include(locate_template('partials/gallery-banner.php'));
+include(locate_template('partials/gallery-quick-links.php'));
+include(locate_template('partials/gallery-archive-search.php'));
+?>
--- source/archive-gallery.php
+++ source/archive-gallery.php
@@ -7,4 +7,6 @@
 get_header();
 
 include(locate_template('partials/gallery-banner.php'));
+include(locate_template('partials/gallery-quick-links.php'));
+include(locate_template('partials/gallery-archive-search.php'));
 ?>

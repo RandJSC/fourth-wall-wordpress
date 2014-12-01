1e52c3b8253c502580dde6626a1600be76dcee19
Mike Green <mike.is.green@gmail.com>
2014-12-01 16:58:17 -0500

    started on a method to get just pin names and coordinates for location api

--- source/includes/api/locations.php
+++ source/includes/api/locations.php
@@ -11,7 +11,27 @@ class FourthWall_Locations extends WP_JSON_CustomPostType {
 
   public function register_routes($routes) {
     $routes = parent::register_routes($routes);
+
+    $routes['/fwe/location-pins'] = array(
+      array(
+        'callback'  => array( $this, 'get_pins' ),
+        'methods'   => WP_JSON_Server::READABLE,
+        'v1_compat' => true,
+      ),
+    );
+
     return $routes;
+  }
+
+  public function get_pins($context = 'view') {
+    $params = array(
+      'post_type'      => 'location',
+      'post_status'    => 'publish',
+      'orderby'        => 'name',
+      'posts_per_page' => -1,
+    );
+
+    // [todo] - get pins via WP_Query and render as WP_JSON_Response instance
   }
 
   protected function prepare_post($post, $context = 'view') {

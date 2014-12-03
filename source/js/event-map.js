20e75b6dee3dbc3387bd1803b04c29d0e0ca1699
Mike Green <mike.is.green@gmail.com>
2014-12-03 14:09:13 -0500

    Only triggering tilesloaded map init handler once

--- source/js/event-map.js
+++ source/js/event-map.js
@@ -9,14 +9,20 @@
 
   'use strict';
 
-  var $       = require('jquery');
-  var config  = require('./lib/config');
-  var logger  = require('bragi-browser');
-  var forEach = require('lodash.foreach');
+  var $          = require('jquery');
+  var config     = require('./lib/config');
+  var logger     = require('bragi-browser');
+  var forEach    = require('lodash.foreach');
+  var Handlebars = require('handlebars');
+
+  var templates  = {
+    markerTitle: Handlebars.compile('{{ title }} ({{ count }})')
+  };
 
   $(document).ready(function() {
     logger.log('eventMap', 'Initializing event map');
 
+    var loaded   = false;
     var $canvas  = $('#map-canvas');
     var data     = $canvas.data();
     var mapOpts  = {
@@ -30,6 +36,10 @@
     var eventMap = new google.maps.Map($canvas[0], mapOpts);
 
     eventMap.addListener('tilesloaded', function(evt) {
+      if (loaded) {
+        return;
+      }
+
       logger.log('eventMap', 'Tiles loaded');
 
       var markersReq = $.ajax(data.pinsUrl, {
@@ -48,15 +58,32 @@
         forEach(json, function(loc) {
           logger.log('eventMap', 'Adding map marker: %s', ('(' + loc.latitude + ', ' + loc.longitude + ')'));
 
-          var latLng = new google.maps.LatLng(loc.latitude, loc.longitude);
-          var marker = new google.maps.Marker({
+          var jsonLink     = loc.links.json;
+          var projectCount = loc.galleries + loc.case_studies;
+          var title        = templates.markerTitle({ title: loc.title, count: projectCount });
+          var latLng       = new google.maps.LatLng(loc.latitude, loc.longitude);
+          var marker       = new google.maps.Marker({
             position: latLng,
             map: eventMap,
-            title: loc.title,
+            title: title,
             animation: google.maps.Animation.DROP
+          });
+
+          google.maps.event.addListener(marker, 'click', function() {
+            logger.log('eventMap', 'Marker clicked: %s', loc.title);
+            var contentReq = $.ajax(jsonLink, {
+              type: 'GET',
+              dataType: 'json'
+            });
+
+            contentReq.success(function(content) {
+              
+            });
           });
         });
       });
+
+      loaded = true;
     });
 
   });

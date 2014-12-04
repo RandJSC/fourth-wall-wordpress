084dd2c50da80b235b8174cc8a4ce855014f58e0
Mike Green <mike.is.green@gmail.com>
2014-12-04 14:30:51 -0500

    Enabled pinch zooming on touch devices

--- source/header.php
+++ source/header.php
@@ -11,7 +11,7 @@ $fwe_settings = get_option('fwe_settings');
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
   <title><?php wp_title(); ?></title>
 

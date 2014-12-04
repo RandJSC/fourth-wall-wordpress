7b7becc5df2b80f71bb089a0d5219e81fc8dac2f
Mike Green <mike.is.green@gmail.com>
2014-12-04 15:42:49 -0500

    started desktop-ifying the header navigation and search

--- source/searchform.php
+++ source/searchform.php
@@ -2,6 +2,7 @@
   <label>
     <span class="screen-reader-text">Search for:</span>
     <input class="search-field" type="search" placeholder="Search &hellip;" name="s" title="Search for:">
+    <span class="fa fa-search"></span>
   </label>
   <input type="submit" class="search-submit" value="Search">
 </form>

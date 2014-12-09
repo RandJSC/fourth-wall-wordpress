abdd850d689cba7972688c4adf81804db7205c47
Mike Green <mike.is.green@gmail.com>
2014-12-09 11:23:55 -0500

    Converted page background fields to a group, so that there can be different background properties for different responsive breakpoints

--- source/piklist/parts/meta-boxes/page-background-image-meta-box.php
+++ source/piklist/parts/meta-boxes/page-background-image-meta-box.php
@@ -7,55 +7,77 @@ Priority: high
 */
 
 piklist('field', array(
-  'type'        => 'file',
-  'field'       => 'background_image',
-  'scope'       => 'post_meta',
-  'label'       => 'Background Image',
-  'description' => 'This image will be used as the page\'s background, stretched to fill the width of the user\'s screen. For best results, use an SVG.',
-  'validate'    => array(
+  'type'     => 'group',
+  'field'    => 'page_backgrounds',
+  'label'    => 'Responsive Backgrounds',
+  'add_more' => true,
+  'columns'  => 12,
+  'fields'   => array(
     array(
-      'type'    => 'limit',
-      'options' => array(
-        'min' => 0,
-        'max' => 1,
+      'type'        => 'number',
+      'field'       => 'min_width',
+      'label'       => 'Minimum Window Width',
+      'description' => 'Window width in pixels. Images with higher minimum widths override lower ones.',
+      'columns'     => 6,
+      'attributes'  => array(
+        'min'  => 0,
+        'max'  => 1920,
+        'step' => 1,
       ),
     ),
-  ),
-));
 
-piklist('field', array(
-  'type'        => 'color',
-  'field'       => 'background_color',
-  'scope'       => 'post_meta',
-  'label'       => 'Background Color',
-  'description' => 'The background color shown if the image is too small for the page. Leave blank for transparent.',
-));
+    array(
+      'type'        => 'color',
+      'field'       => 'background_color',
+      'label'       => 'Background Color',
+      'description' => 'The background color shown if the image is too small for the page. Leave blank for transparent.',
+      'columns'     => 2,
+    ),
 
-piklist('field', array(
-  'type'        => 'text',
-  'field'       => 'background_size',
-  'scope'       => 'post_meta',
-  'label'       => 'Background Size',
-  'description' => 'Supports any valid CSS value for <code>background-size</code>. This can be used to "zoom" a background image to a specific size. Leave blank for a default 1X zoom level.',
-));
+    array(
+      'type'        => 'file',
+      'field'       => 'background_image',
+      'label'       => 'Background Image',
+      'description' => 'This image will be used as the page\'s background, stretched to fill the width of the user\'s screen. For best results, use an SVG.',
+      'columns'     => 12,
+      'validate'    => array(
+        array(
+          'type' => 'limit',
+          'options' => array(
+            'min' => 0,
+            'max' => 1,
+          ),
+        ),
+      ),
+    ),
 
-piklist('field', array(
-  'type'        => 'text',
-  'field'       => 'background_position',
-  'scope'       => 'post_meta',
-  'label'       => 'Background Position',
-  'description' => 'Supports any valid CSS value for <code>background-position</code>. Leave blank to center the image.'
-));
+    array(
+      'type'        => 'text',
+      'field'       => 'background_size',
+      'label'       => 'Background Size',
+      'description' => 'Supports any valid CSS value for <code>background-size</code>. This can be used to "zoom" a background image to a specific size. Leave blank for a default 1X zoom level.',
+      'columns'     => 12,
+    ),
 
-piklist('field', array(
-  'type'        => 'radio',
-  'field'       => 'background_repeat',
-  'scope'       => 'post_meta',
-  'label'       => 'Background Repeat',
-  'description' => 'Is the background image a repeating pattern?',
-  'choices'     => array(
-    'repeat'    => 'Repeat',
-    'no-repeat' => 'No Repeat'
+    array(
+      'type'        => 'text',
+      'field'       => 'background_position',
+      'label'       => 'Background Position',
+      'description' => 'Supports any valid CSS value for <code>background-position</code>. Leave blank to center the image.',
+      'columns'     => 12,
+    ),
+
+    array(
+      'type'        => 'radio',
+      'field'       => 'background_repeat',
+      'label'       => 'Background Repeat',
+      'description' => 'Is the background image a repeating pattern?',
+      'columns'     => 12,
+      'choices'     => array(
+        'repeat'    => 'Repeat',
+        'no-repeat' => 'No Repeat',
+      ),
+    ),
   ),
 ));
 

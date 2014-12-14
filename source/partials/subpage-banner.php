ad1c74145c4a675d5f989869e469bf37921c30c8
Mike Green <mike.is.green@gmail.com>
2014-12-13 21:03:16 -0500

    Added ability for page-banner to have different styles based on parent page

--- source/partials/subpage-banner.php
+++ source/partials/subpage-banner.php
@@ -1,8 +1,18 @@
+<?php
+global $post;
+
+if (fwe_is_post($post)) {
+  $parent  = fwe_get_root_parent($post);
+  $classes = ' ' . $parent->post_name;
+} else {
+  $classes = '';
+}
+?>
 <?php if ($banner && $banner['banner']): ?>
   <?php
   $banner_src = wp_get_attachment_image_src($banner['banner'], 'full');
   ?>
-  <section class="banner">
+  <section class="banner<?php echo $classes; ?>">
     <figure id="page-banner">
       <img src="<?php echo $banner_src[0]; ?>" alt="<?php echo $banner['caption']; ?>" width="100%">
       <figcaption><?php echo $banner['caption']; ?></figcaption>

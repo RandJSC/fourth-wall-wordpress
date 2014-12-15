bce07249fab38a7fc77e09ca4872e264e44fbe1e
Mike Green <mike.is.green@gmail.com>
2014-12-15 05:43:28 -0500

    Added given name field to team members

--- source/piklist/parts/meta-boxes/team-member-meta-box.php
+++ source/piklist/parts/meta-boxes/team-member-meta-box.php
@@ -8,6 +8,16 @@ Priority: high
 
 piklist('field', array(
   'type'       => 'text',
+  'field'      => 'given_name',
+  'label'      => 'Given (First) Name',
+  'help'       => 'If you leave this blank, we\'ll try to guess it from the team member\'s full name.',
+  'attributes' => array(
+    'class' => 'text',
+  ),
+));
+
+piklist('field', array(
+  'type'       => 'text',
   'field'      => 'surname',
   'label'      => 'Surname',
   'help'       => 'If you leave this blank, we\'ll try to guess it from the team member\'s full name.',

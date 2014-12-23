4d6204ed146fe4f0426de51585cfdfc70d1d85db
Mike Green <mike.is.green@gmail.com>
2014-12-23 15:01:57 -0500

    Added MailChimp settings to admin theme options, conditionally showing a list selection field if a valid API key is saved

---
+++ source/piklist/parts/settings/mailchimp-settings.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Title: MailChimp Settings
+Setting: fwe_settings
+Tab: MailChimp
+*/
+
+// Load MailChimp API wrapper
+$theme_dir = trailingslashit(dirname(__FILE__)) . '../../../';
+require_once $theme_dir . 'includes/vendor/autoload.php';
+
+$fwe_settings = get_option('fwe_settings');
+
+piklist('field', array(
+  'type'        => 'text',
+  'field'       => 'mailchimp_api_key',
+  'label'       => 'MailChimp API Key',
+  'description' => 'You can find this by viewing your profile in MailChimp and selecting Extras &gt; API Keys',
+  'value'       => 'cbb2a7e4d7c04540270ae1d494efe0c0-us8',
+));
+
+if (array_key_exists('mailchimp_api_key', $fwe_settings) && $fwe_settings['mailchimp_api_key']) {
+  $mc    = new \Drewm\MailChimp($fwe_settings['mailchimp_api_key']);
+  $lists = $mc->call('lists/list');
+
+  $list_choices = array('' => '-- Select a List --');
+
+  foreach ($lists['data'] as $list) {
+    $list_choices[$list['id']] = $list['name'];
+  }
+
+  piklist('field', array(
+    'type'    => 'select',
+    'field'   => 'mailchimp_list_id',
+    'label'   => 'Subscriber List',
+    'choices' => $list_choices,
+  ));
+}
+?>

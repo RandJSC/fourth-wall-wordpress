f25e4764d22a3c1334010c601bbc74a9705f1590
Mike Green <mike.is.green@gmail.com>
2015-02-04 14:09:53 -0500

    Moved contact info into add_more group for multiple locations

--- source/piklist/parts/settings/company-info-settings.php
+++ source/piklist/parts/settings/company-info-settings.php
@@ -18,40 +18,21 @@ piklist('field', array(
 ));
 
 piklist('field', array(
-  'type'        => 'text',
-  'field'       => 'address',
-  'label'       => 'Street Address',
-  'description' => 'Include second line (suite number, etc.)'
 ));
 
 piklist('field', array(
-  'type'  => 'text',
-  'field' => 'city',
-  'label' => 'City',
 ));
 
 piklist('field', array(
-  'type'  => 'text',
-  'field' => 'state',
-  'label' => 'State',
 ));
 
 piklist('field', array(
-  'type'  => 'text',
-  'field' => 'zip_code',
-  'label' => 'Zip Code',
 ));
 
 piklist('field', array(
-  'type'  => 'text',
-  'field' => 'phone',
-  'label' => 'Phone Number',
 ));
 
 piklist('field', array(
-  'type'  => 'text',
-  'field' => 'contact_email',
-  'label' => 'Contact Email',
 ));
 
 piklist('field', array(
@@ -60,4 +41,56 @@ piklist('field', array(
   'label' => 'Google Maps Link',
   'value' => 'https://www.google.com/maps/place/Fourth+Wall+Events/@40.755227,-73.988349,15z/data=!4m2!3m1!1s0x0:0x9051d405614e4d61',
 ));
+
+piklist('field', array(
+  'type'     => 'group',
+  'field'    => 'locations',
+  'label'    => 'Locations',
+  'add_more' => true,
+  'fields'   => array(
+    array(
+      'type'    => 'text',
+      'field'   => 'location_name',
+      'label'   => 'Location Name',
+      'columns' => 12,
+    ),
+    array(
+      'type'        => 'text',
+      'field'       => 'address',
+      'label'       => 'Street Address',
+      'description' => 'Include second line (suite number, etc.)',
+      'columns'     => 12,
+    ),
+    array(
+      'type'    => 'text',
+      'field'   => 'city',
+      'label'   => 'City',
+      'columns' => 5,
+    ),
+    array(
+      'type'    => 'text',
+      'field'   => 'state',
+      'label'   => 'State',
+      'columns' => 7,
+    ),
+    array(
+      'type'    => 'text',
+      'field'   => 'zip_code',
+      'label'   => 'Zip Code',
+      'columns' => 3,
+    ),
+    array(
+      'type'    => 'text',
+      'field'   => 'phone',
+      'label'   => 'Phone Number',
+      'columns' => 9,
+    ),
+    array(
+      'type'    => 'text',
+      'field'   => 'contact_email',
+      'label'   => 'Contact Email',
+      'columns' => 12,
+    ),
+  ),
+))
 ?>

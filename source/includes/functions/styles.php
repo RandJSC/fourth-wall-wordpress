c4e4f0713a7794548a1312142f1cc58ce8094a44
Mike Green <mike.is.green@gmail.com>
2015-02-10 07:58:49 -0500

    Removed unused stylesheet requirements from styles.php

--- source/includes/functions/styles.php
+++ source/includes/functions/styles.php
@@ -7,28 +7,10 @@
 function fwe_enqueue_styles() {
   $theme_uri = get_stylesheet_directory_uri();
   $styles    = array(
-    'normalize' => array(
-      'src'     => "$theme_uri/css/normalize.css",
-      'deps'    => false,
-      'version' => '3.0.1',
-      'media'   => 'all',
-    ),
-    'font-awesome' => array(
-      'src'     => "$theme_uri/css/font-awesome.min.css",
-      'deps'    => false,
-      'version' => '4.1.0',
-      'media'   => 'all',
-    ),
     'source-sans-pro' => array(
       'src'     => '//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,400italic,600italic',
       'deps'    => false,
       'version' => '0.1.0',
-      'media'   => 'all',
-    ),
-    'slick-carousel' => array(
-      'src'     => "$theme_uri/css/slick.css",
-      'deps'    => false,
-      'version' => '1.3.7',
       'media'   => 'all',
     ),
     'fwe-main' => array(
